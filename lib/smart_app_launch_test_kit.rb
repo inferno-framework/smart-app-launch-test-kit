98a8a09c6b5e1bfa9a8ac85b1a016a5b52d69039
Stephen MacVicar <smacvicar@mitre.org>
2022-02-04 14:26:24 -0500

    differentiate standalone and ehr credentials

--- lib/smart_app_launch_test_kit.rb
+++ lib/smart_app_launch_test_kit.rb
@@ -58,7 +58,8 @@ module SMARTAppLaunch
                 id_token: { name: :standalone_id_token },
                 client_id: { name: :standalone_client_id },
                 requested_scopes: { name: :standalone_requested_scopes },
-                access_token: { name: :standalone_access_token }
+                access_token: { name: :standalone_access_token },
+                smart_credentials: { name: :standalone_smart_credentials }
               }
             }
 
@@ -77,7 +78,8 @@ module SMARTAppLaunch
                 received_scopes: { name: :standalone_received_scopes },
                 access_token: { name: :standalone_access_token },
                 token_retrieval_time: { name: :standalone_token_retrieval_time },
-                expires_in: { name: :standalone_expires_in }
+                expires_in: { name: :standalone_expires_in },
+                smart_credentials: { name: :standalone_smart_credentials }
               }
             }
 
@@ -97,7 +99,8 @@ module SMARTAppLaunch
                 received_scopes: { name: :standalone_received_scopes },
                 access_token: { name: :standalone_access_token },
                 token_retrieval_time: { name: :standalone_token_retrieval_time },
-                expires_in: { name: :standalone_expires_in }
+                expires_in: { name: :standalone_expires_in },
+                smart_credentials: { name: :standalone_smart_credentials }
               }
             }
     end
@@ -118,7 +121,8 @@ module SMARTAppLaunch
                 id_token: { name: :ehr_id_token },
                 client_id: { name: :ehr_client_id },
                 requested_scopes: { name: :ehr_requested_scopes },
-                access_token: { name: :ehr_access_token }
+                access_token: { name: :ehr_access_token },
+                smart_credentials: { name: :ehr_smart_credentials }
               }
             }
 
@@ -137,7 +141,8 @@ module SMARTAppLaunch
                 received_scopes: { name: :ehr_received_scopes },
                 access_token: { name: :ehr_access_token },
                 token_retrieval_time: { name: :ehr_token_retrieval_time },
-                expires_in: { name: :ehr_expires_in }
+                expires_in: { name: :ehr_expires_in },
+                smart_credentials: { name: :ehr_smart_credentials }
               }
             }
 
@@ -157,7 +162,8 @@ module SMARTAppLaunch
                 received_scopes: { name: :ehr_received_scopes },
                 access_token: { name: :ehr_access_token },
                 token_retrieval_time: { name: :ehr_token_retrieval_time },
-                expires_in: { name: :ehr_expires_in }
+                expires_in: { name: :ehr_expires_in },
+                smart_credentials: { name: :ehr_smart_credentials }
               }
             }
     end

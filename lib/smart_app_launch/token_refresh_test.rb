58e8b1cbc31331e95ce637b88216ff2f23889d61
Stephen MacVicar <smacvicar@mitre.org>
2022-02-04 13:57:23 -0500

    use oauth_credentials

--- lib/smart_app_launch/token_refresh_test.rb
+++ lib/smart_app_launch/token_refresh_test.rb
@@ -18,6 +18,7 @@ module SMARTAppLaunch
     )
     input :well_known_token_url, :refresh_token, :client_id, :received_scopes
     input :client_secret, optional: true
+    output :smart_credentials, :token_retrieval_time
     makes_request :token_refresh
 
     run do
@@ -41,7 +42,20 @@ module SMARTAppLaunch
       post(well_known_token_url, body: oauth2_params, name: :token_refresh, headers: oauth2_headers)
 
       assert_response_status(200)
-      assert_valid_json(response[:body])
+      assert_valid_json(request.response_body)
+
+      output token_retrieval_time: Time.now.iso8601
+
+      token_response_body = JSON.parse(request.response_body)
+      output smart_credentials: {
+               refresh_token: token_response_body['refresh_token'],
+               access_token: token_response_body['access_token'],
+               expires_in: token_response_body['expires_in'],
+               client_id: client_id,
+               client_secret: client_secret,
+               token_retrieval_time: token_retrieval_time,
+               token_url: well_known_token_url
+             }.to_json
     end
   end
 end

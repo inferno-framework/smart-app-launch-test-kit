62f680822abfed40e7edb9d7a45cafbbc34fc2f0
radamson <radamson@users.noreply.github.com>
2022-06-13 15:48:40 -0400

    update require_relative path

--- lib/smart_app_launch_test_kit.rb
+++ lib/smart_app_launch_test_kit.rb
@@ -1,4 +1,4 @@
 require 'tls_test_kit'
 
-require_relative 'smart_stu1_suite'
-require_relative 'smart_stu2_suite'
+require_relative 'smart_app_launch/smart_stu1_suite'
+require_relative 'smart_app_launch/smart_stu2_suite'

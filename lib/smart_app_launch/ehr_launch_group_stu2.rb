d6a3421a02bd60a073bcb4455424718218357e4d
Stephen MacVicar <Jammjammjamm@users.noreply.github.com>
2022-06-24 09:32:23 -0400

    FI-1565: Require PKCE for STU2 launches (#24)
    * update gemfile.lock for arm mac
    
    * add additional redis directory to gitignore
    
    * require pkce with S256 for stu2 suite
    
    * make stu2 launch groups inherit from stu1 groups
    
    * update standalone/ehr launch links for stu2

---
+++ lib/smart_app_launch/ehr_launch_group_stu2.rb
@@ -0,0 +1,45 @@
+require_relative 'ehr_launch_group'
+
+module SMARTAppLaunch
+  class EHRLaunchGroupSTU2 < EHRLaunchGroup
+    id :smart_ehr_launch_stu2
+    description %(
+      # Background
+
+      The [EHR
+      Launch](http://hl7.org/fhir/smart-app-launch/STU2/app-launch.html#launch-app-ehr-launch)
+      is one of two ways in which an app can be launched, the other being
+      Standalone launch. In an EHR launch, the app is launched from an
+      existing EHR session or portal by a redirect to the registered launch
+      URL. The EHR provides the app two parameters:
+
+      * `iss` - Which contains the FHIR server url
+      * `launch` - An identifier needed for authorization
+
+      # Test Methodology
+
+      Inferno will wait for the EHR server redirect upon execution. When the
+      redirect is received Inferno will check for the presence of the `iss`
+      and `launch` parameters. The security of the authorization endpoint is
+      then checked and authorization is attempted using the provided `launch`
+      identifier.
+
+      For more information on the #{title} see:
+
+      * [SMART EHR Launch Sequence](http://hl7.org/fhir/smart-app-launch/STU2/app-launch.html#launch-app-ehr-launch)
+    )
+
+    config(
+      inputs: {
+        use_pkce: {
+          default: 'true',
+          locked: true
+        },
+        pkce_code_challenge_method: {
+          default: 'S256',
+          locked: true
+        }
+      }
+    )
+  end
+end

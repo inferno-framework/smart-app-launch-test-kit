7dc0bcf55b6ea15f7810521f3c60e532cbcc19e5
Stephen MacVicar <Jammjammjamm@users.noreply.github.com>
2023-01-23 11:03:36 -0500

    Update launch test description (#41)

--- lib/smart_app_launch/launch_received_test.rb
+++ lib/smart_app_launch/launch_received_test.rb
@@ -2,7 +2,8 @@ module SMARTAppLaunch
   class LaunchReceivedTest < Inferno::Test
     title 'EHR server sends launch parameter'
     description %(
-      Code is a required querystring parameter on the redirect.
+      The `launch` URL parameter associates the app's authorization request with
+      the current EHR session.
     )
     id :smart_launch_received
 

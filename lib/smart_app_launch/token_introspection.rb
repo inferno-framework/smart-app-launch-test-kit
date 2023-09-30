c8b87b77f7891e689029ca5d498e5f35f303b2a2
Alisa Wallace <awallace@mitre.org>
2023-09-30 16:49:10 -0700

    Initial attempt at token introspection tests

---
+++ lib/smart_app_launch/token_introspection.rb
@@ -0,0 +1,32 @@
+module SMARTAppLaunch
+  class TokenIntrospectionTestGroup < Inferno::TestGroup
+    title 'Token Introspection Group'
+    description %(TODO)
+    id :token_introspection_test_group
+
+    test do
+      title 'Token introspection endpoint returns correct response for valid token'
+      input :token_introspection_base_url,
+            :token_introspection_endpoint,
+            :client_id
+      input :client_secret, optional: true
+      input :standalone_access_token
+      output :token_introspection_url
+
+      http_client do
+        url :token_introspection_base_url
+      end
+
+      run do
+        headers = {'Accept' => 'application/json', 'Content-Type' => 'application/x-www-form-urlencoded'}
+        body = "token=#{standalone_access_token}"
+        post(token_introspection_endpoint, body: body, headers: headers)
+        assert_response_status(200)
+      end
+    end
+
+    test do 
+      title 'Token introspection endpoint returns correct response for invalid token'
+    end
+  end
+end

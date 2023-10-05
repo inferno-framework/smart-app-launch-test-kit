65c3e07c2de1a41e261802c4105860044b14c56d
Alisa Wallace <awallace@mitre.org>
2023-10-05 15:54:15 -0400

    Basic Keycloak auth server added to Docker

--- lib/smart_app_launch/token_introspection.rb
+++ lib/smart_app_launch/token_introspection.rb
@@ -6,6 +6,7 @@ module SMARTAppLaunch
 
     test do
       title 'Token introspection endpoint returns correct response for valid token'
+      # base url is really just auth server base url I believe
       input :token_introspection_base_url,
             :token_introspection_endpoint,
             :client_id
@@ -27,6 +28,24 @@ module SMARTAppLaunch
 
     test do 
       title 'Token introspection endpoint returns correct response for invalid token'
+      # TODO fix duplicated code
+      input :token_introspection_base_url,
+            :token_introspection_endpoint,
+            :client_id
+      input :client_secret, optional: true
+      input :standalone_access_token
+      output :token_introspection_url
+
+      http_client do
+        url :token_introspection_base_url
+      end
+
+      run do
+        headers = {'Accept' => 'application/json', 'Content-Type' => 'application/x-www-form-urlencoded'}
+        body = "token=#{'fake_token_value'}"
+        post(token_introspection_endpoint, body: body, headers: headers)
+        assert_response_status(401)
+      end
     end
   end
 end

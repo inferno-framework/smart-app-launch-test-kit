a351c6c68ee6d1c9a0617f05992dafb618f89b31
Alisa Wallace <awallace@mitre.org>
2023-10-09 10:57:11 -0700

    Check for correct values in token introspection response

--- lib/smart_app_launch/token_introspection.rb
+++ lib/smart_app_launch/token_introspection.rb
@@ -6,7 +6,6 @@ module SMARTAppLaunch
 
     test do
       title 'Token introspection endpoint returns correct response for valid token'
-      # base url is really just auth server base url I believe
       input :token_introspection_base_url,
             :token_introspection_endpoint,
             :client_id
@@ -19,6 +18,18 @@ module SMARTAppLaunch
       end
 
       run do
+        begin
+          payload, header =
+            JWT.decode(
+              standalone_access_token,
+              nil,
+              false
+            )
+            # puts "Access token payload = #{payload}}"
+        rescue StandardError => e
+          assert false, "Access token is not a properly constructed JWT: #{e.message}"
+        end
+
         headers = {'Accept' => 'application/json', 'Content-Type' => 'application/x-www-form-urlencoded'}
         body = "token=#{standalone_access_token}"
         unless client_secret.blank?
@@ -27,6 +38,19 @@ module SMARTAppLaunch
         puts 'Body = ' + body
         post(token_introspection_endpoint, body: body, headers: headers)
         assert_response_status(200)
+        assert_valid_json(request.response_body)
+
+        # check contents
+        introspection_response_body = JSON.parse(request.response_body)
+        active_value = introspection_response_body['active']
+        client_id_value = introspection_response_body['client_id']
+        scope_value = introspection_response_body['scope']
+        exp_value = introspection_response_body['exp']
+
+        assert active_value == true, 'Failure: active not set to true for valid token'
+        assert client_id_value == payload['client_id'], 'Failure: client_id field does not match between introspection response and original access token'
+        assert scope_value == payload['scope'], 'Failure: scope field does not match between introspection response and original access token'
+        assert exp_value == payload['exp'], 'Failure: exp field does not match between introspection response and original access token'
       end
     end
 

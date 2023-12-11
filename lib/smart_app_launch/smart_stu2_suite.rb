0cc043b34301a834cb6f8823e17a64dcc069573d
Alisa Wallace <awallace@mitre.org>
2023-12-11 15:05:39 -0800

    First pass at backend services refactor, page loads but tests fail

--- lib/smart_app_launch/smart_stu2_suite.rb
+++ lib/smart_app_launch/smart_stu2_suite.rb
@@ -8,6 +8,7 @@ require_relative 'ehr_launch_group_stu2'
 require_relative 'openid_connect_group'
 require_relative 'token_introspection_group'
 require_relative 'token_refresh_group'
+require_relative 'backend_services_group'
 
 module SMARTAppLaunch
   class SMARTSTU2Suite < Inferno::TestSuite
@@ -221,6 +222,8 @@ module SMARTAppLaunch
     end
 
     group from: :smart_token_introspection
+
+    group from: :smart_backend_services
 
   end
 end

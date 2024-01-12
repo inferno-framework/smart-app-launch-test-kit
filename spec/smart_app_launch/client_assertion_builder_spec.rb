ecf8270bb357538d3dbf2215c0d0662f939128f2
Alisa Wallace <awallace@mitre.org>
2024-01-11 17:10:08 -0800

    WIP refactored ClientAssertionBuilder spec tests

--- spec/smart_app_launch/client_assertion_builder_spec.rb
+++ spec/smart_app_launch/client_assertion_builder_spec.rb
@@ -6,18 +6,39 @@ RSpec.describe SMARTAppLaunch::ClientAssertionBuilder do
   let(:iss) { 'ISS' }
   let(:sub) { 'SUB' }
   let(:aud) { 'AUD' }
-  let(:kid) { nil }
   let(:jwks) { SMARTAppLaunch::JWKS.jwks }
 
+  def build_and_decode_jwt(encryption_method, kid)
+    jwt = described_class.build(client_auth_encryption_method: encryption_method, iss:, sub:, aud:, kid: kid)
+    return JWT.decode(jwt, kid, true, algorithms: [encryption_method], jwks:)
+  end
+
   describe '.build' do
-    it 'creates a valid JWT' do
-      client_auth_encryption_methods.each do |client_auth_encryption_method|
-        jwt = described_class.build(client_auth_encryption_method:, iss:, sub:, aud:, kid:)
+    context 'with unspecified key id' do
+      it 'creates a valid JWT' do
+        client_auth_encryption_methods.each do |client_auth_encryption_method|
+          payload, header = build_and_decode_jwt(client_auth_encryption_method, nil)
 
-        payload, header = JWT.decode(jwt, nil, true, algorithms: [client_auth_encryption_method], jwks:)
+          expect(header['alg']).to eq(client_auth_encryption_method)
+          expect(header['typ']).to eq('JWT')
+          expect(payload['iss']).to eq(iss)
+          expect(payload['sub']).to eq(sub)
+          expect(payload['aud']).to eq(aud)
+          expect(payload['exp']).to be_present
+          expect(payload['jti']).to be_present
+        end
+      end
+    end
 
-        expect(header['alg']).to eq(client_auth_encryption_method)
+    context 'with specified key id' do
+      it 'creates a valid JWT with correct algorithm and kid' do
+        encryption_method = 'ES384'
+        kid = '4b49a739d1eb115b3225f4cf9beb6d1b'
+        payload, header = build_and_decode_jwt(encryption_method, kid)
+
+        expect(header['alg']).to eq(encryption_method)
         expect(header['typ']).to eq('JWT')
+        expect(header['kid']).to eq(kid)
         expect(payload['iss']).to eq(iss)
         expect(payload['sub']).to eq(sub)
         expect(payload['aud']).to eq(aud)

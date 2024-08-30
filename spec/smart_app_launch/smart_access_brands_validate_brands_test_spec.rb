ef3de91b26820689af1bc9519d6507f22d17c83b
Emily Michaud <59289146+emichaud998@users.noreply.github.com>
2024-08-29 20:24:56 -0400

    Add spec tests for User Access Brands tests

---
+++ spec/smart_app_launch/smart_access_brands_validate_brands_test_spec.rb
@@ -0,0 +1,173 @@
+require_relative '../../lib/smart_app_launch/smart_access_brands_retrieve_bundle_test'
+
+RSpec.describe SMARTAppLaunch::SMARTAccessBrandsValidateBrands do
+  let(:suite) { Inferno::Repositories::TestSuites.new.find('smart_access_brands') }
+  let(:session_data_repo) { Inferno::Repositories::SessionData.new }
+  let(:results_repo) { Inferno::Repositories::Results.new }
+  let(:test_session) { repo_create(:test_session, test_suite_id: 'smart_access_brands') }
+  let(:result) { repo_create(:result, test_session_id: test_session.id) }
+
+  let(:smart_access_brands_bundle) do
+    JSON.parse(File.read(File.join(
+                           __dir__, '..', 'fixtures', 'smart_access_brands_example.json'
+                         )))
+  end
+
+  let(:operation_outcome_success) do
+    {
+      outcomes: [{
+        issues: []
+      }],
+      sessionId: 'b8cf5547-1dc7-4714-a797-dc2347b93fe2'
+    }
+  end
+
+  let(:operation_outcome_failure) do
+    {
+      outcomes: [{
+        issues: [{
+          level: 'ERROR',
+          message: 'Resource does not conform to profile'
+        }]
+      }],
+      sessionId: 'b8cf5547-1dc7-4714-a797-dc2347b93fe2'
+    }
+  end
+
+  let(:validator_url) { ENV.fetch('FHIR_RESOURCE_VALIDATOR_URL') }
+  let(:smart_access_brands_bundle_url) { 'http://fhirserver.org/smart_access_brands_example.json' }
+
+  def create_user_access_brands_request(url: smart_access_brands_bundle_url, body: nil, status: 200)
+    repo_create(
+      :request,
+      name: 'retrieve_smart_access_brands_bundle',
+      direction: 'outgoing',
+      url:,
+      result:,
+      test_session_id: test_session.id,
+      response_body: body.is_a?(Hash) ? body.to_json : body,
+      status:,
+      tags: ['smart_access_brands_bundle']
+    )
+  end
+
+  def run(runnable, inputs = {})
+    test_run_params = { test_session_id: test_session.id }.merge(runnable.reference_hash)
+    test_run = Inferno::Repositories::TestRuns.new.create(test_run_params)
+    inputs.each do |name, value|
+      session_data_repo.save(
+        test_session_id: test_session.id,
+        name:,
+        value:,
+        type: runnable.config.input_type(name) || 'text'
+      )
+    end
+    Inferno::TestRunner.new(test_session:, test_run:).run(runnable)
+  end
+
+  describe 'SMART Access Brands Validate Brands Test' do
+    let(:test) do
+      Class.new(SMARTAppLaunch::SMARTAccessBrandsValidateBrands) do
+        fhir_resource_validator do
+          url ENV.fetch('FHIR_RESOURCE_VALIDATOR_URL')
+
+          cli_context do
+            txServer nil
+            displayWarnings true
+            disableDefaultResourceFetcher true
+          end
+
+          igs('hl7.fhir.uv.smart-app-launch#2.2.0')
+        end
+      end
+    end
+
+    it 'passes if User Access Brands Bundle contains valid Brands' do
+      validation_request = stub_request(:post, "#{validator_url}/validate")
+        .to_return(status: 200, body: operation_outcome_success.to_json)
+
+      create_user_access_brands_request(body: smart_access_brands_bundle)
+
+      result = run(test)
+
+      expect(result.result).to eq('pass')
+      expect(validation_request).to have_been_made
+    end
+
+    it 'skips if no User Access Brands Bundle requests were made' do
+      result = run(test)
+
+      expect(result.result).to eq('skip')
+      expect(result.result_message).to eq('No SMART Access Brands request was made in the previous test.')
+    end
+
+    it 'skips if User Access Brands Bundle request does not contain a response body' do
+      create_user_access_brands_request
+      result = run(test)
+
+      expect(result.result).to eq('skip')
+      expect(result.result_message).to eq('No SMART Access Brands Bundle contained in the response')
+    end
+
+    it 'fails if User Access Brands Bundle is an invalid JSON' do
+      create_user_access_brands_request(body: '[[')
+
+      result = run(test)
+
+      expect(result.result).to eq('fail')
+      expect(result.result_message).to eq('Invalid JSON. ')
+    end
+
+    it 'skips if User Access Brands Bundle is empty' do
+      smart_access_brands_bundle['entry'] = []
+      create_user_access_brands_request(body: smart_access_brands_bundle)
+      result = run(test)
+
+      expect(result.result).to eq('skip')
+      expect(result.result_message).to eq('The given Bundle does not contain any resources')
+    end
+
+    it "fails if the User Access Brands Bundle's contained Brands fail validation" do
+      validation_request = stub_request(:post, "#{validator_url}/validate")
+        .to_return(status: 200, body: operation_outcome_failure.to_json)
+
+      create_user_access_brands_request(body: smart_access_brands_bundle)
+
+      result = run(test)
+
+      expect(result.result).to eq('fail')
+      expect(result.result_message).to eq('The following bundle entries are invalid: Organization#examplehospital')
+      expect(validation_request).to have_been_made
+    end
+
+    it 'fails if Brand contains Endpoint in portal extension but not Organization.endpoint' do
+      validation_request = stub_request(:post, "#{validator_url}/validate")
+        .to_return(status: 200, body: operation_outcome_success.to_json)
+
+      smart_access_brands_bundle['entry'].first['resource']['endpoint'].shift
+      create_user_access_brands_request(body: smart_access_brands_bundle)
+
+      result = run(test)
+
+      expect(result.result).to eq('fail')
+      expect(result.result_message).to match('Portal endpoints must also appear at Organization.endpoint')
+      expect(validation_request).to have_been_made
+    end
+
+    it 'fails if Brand contains Endpoint reference not found in Bundle' do
+      validation_request = stub_request(:post, "#{validator_url}/validate")
+        .to_return(status: 200, body: operation_outcome_success.to_json)
+
+      smart_access_brands_bundle['entry'].delete_at(1)
+      create_user_access_brands_request(body: smart_access_brands_bundle)
+
+      result = run(test)
+
+      expect(result.result).to eq('fail')
+      expect(result.result_message).to match(
+        'Organization with id: examplehospital references an Endpoint that is not contained'
+      )
+      expect(validation_request).to have_been_made
+    end
+  end
+end

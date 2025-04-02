a650e8d08b7a852973d6ebe9fd7690c538b98dd0
Karl Naden <knaden@mitre.org>
2025-04-02 11:32:26 -0400

    variable naming cleanup

--- lib/smart_app_launch/client_suite/client_token_request_verification_test.rb
+++ lib/smart_app_launch/client_suite/client_token_request_verification_test.rb
@@ -43,8 +43,8 @@ module SMARTAppLaunch
       jti_list = []
       requests.each_with_index do |token_request, index|
         request_params = URI.decode_www_form(token_request.request_body).to_h
-        check_request_params(request_params, index)
-        check_client_assertion(request_params['client_assertion'], index, jti_list, token_request.url)
+        check_request_params(request_params, index + 1)
+        check_client_assertion(request_params['client_assertion'], index + 1, jti_list, token_request.url)
       end
 
       assert messages.none? { |msg|
@@ -52,82 +52,82 @@ module SMARTAppLaunch
       }, 'Invalid token requests detected. See messages for details.'
     end
 
-    def check_request_params(params, index)
+    def check_request_params(params, request_num)
       if params['grant_type'] != 'client_credentials'
         add_message('error',
-                    "Token request #{index} had an incorrect `grant_type`: expected 'client_credentials', " \
+                    "Token request #{request_num} had an incorrect `grant_type`: expected 'client_credentials', " \
                     "but got '#{params['grant_type']}'")
       end
       if params['client_assertion_type'] != 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer'
         add_message('error',
-                    "Token request #{index} had an incorrect `client_assertion_type`: " \
+                    "Token request #{request_num} had an incorrect `client_assertion_type`: " \
                     "expected 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer', " \
                     "but got '#{params['client_assertion_type']}'")
       end
       return unless params['scope'].blank?
 
-      add_message('error', "Token request #{index} did not include the requested `scope`")
+      add_message('error', "Token request #{request_num} did not include the requested `scope`")
     end
 
-    def check_client_assertion(assertion, index, jti_list, endpoint_aud)
+    def check_client_assertion(assertion, request_num, jti_list, endpoint_aud)
       decoded_token =
         begin
           JWT::EncodedToken.new(assertion)
         rescue StandardError => e
-          add_message('error', "Token request #{index} contained an invalid client assertion jwt: #{e}")
+          add_message('error', "Token request #{request_num} contained an invalid client assertion jwt: #{e}")
           nil
         end
 
       return unless decoded_token.present?
 
-      check_jwt_header(decoded_token.header, index)
-      check_jwt_payload(decoded_token.payload, index, jti_list, endpoint_aud)
-      check_jwt_signature(decoded_token, index)
+      check_jwt_header(decoded_token.header, request_num)
+      check_jwt_payload(decoded_token.payload, request_num, jti_list, endpoint_aud)
+      check_jwt_signature(decoded_token, request_num)
     end
 
-    def check_jwt_header(header, index)
+    def check_jwt_header(header, request_num)
       return unless header['typ'] != 'JWT'
 
-      add_message('error', "client assertion jwt on token request #{index} has an incorrect `typ` header: " \
+      add_message('error', "client assertion jwt on token request #{request_num} has an incorrect `typ` header: " \
                            "expected 'JWT', got '#{header['typ']}'")
     end
 
-    def check_jwt_payload(claims, index, jti_list, endpoint_aud)
+    def check_jwt_payload(claims, request_num, jti_list, endpoint_aud)
       if claims['iss'] != client_id
-        add_message('error', "client assertion jwt on token request #{index} has an incorrect `iss` claim: " \
+        add_message('error', "client assertion jwt on token request #{request_num} has an incorrect `iss` claim: " \
                              "expected '#{client_id}', got '#{claims['iss']}'")
       end
 
       if claims['sub'] != client_id
-        add_message('error', "client assertion jwt on token request #{index} has an incorrect `sub` claim: " \
+        add_message('error', "client assertion jwt on token request #{request_num} has an incorrect `sub` claim: " \
                              "expected '#{client_id}', got '#{claims['sub']}'")
       end
 
       if claims['aud'] != endpoint_aud
-        add_message('error', "client assertion jwt on token request #{index} has an incorrect `aud` claim: " \
+        add_message('error', "client assertion jwt on token request #{request_num} has an incorrect `aud` claim: " \
                              "expected '#{endpoint_aud}', got '#{claims['aud']}'")
       end
 
       if claims['exp'].blank?
-        add_message('error', "client assertion jwt on token request #{index} is missing the `exp` claim.")
+        add_message('error', "client assertion jwt on token request #{request_num} is missing the `exp` claim.")
       end
 
       if claims['jti'].blank?
-        add_message('error', "client assertion jwt on token request #{index} is missing the `jti` claim.")
+        add_message('error', "client assertion jwt on token request #{request_num} is missing the `jti` claim.")
       elsif jti_list.include?(claims['jti'])
-        add_message('error', "client assertion jwt on token request #{index} has a `jti` claim that was " \
+        add_message('error', "client assertion jwt on token request #{request_num} has a `jti` claim that was " \
                              "previouly used: '#{claims['jti']}'.")
       else
         jti_list << claims['jti']
       end
     end
 
-    def check_jwt_signature(encoded_token, index)
+    def check_jwt_signature(encoded_token, request_num)
       error = MockSMARTServer.smart_assertion_signature_verification(encoded_token, smart_jwk_set)
 
       return unless error.present?
 
-      add_message('error', "Signature validation failed on token request #{index}: #{error}")
+      add_message('error', "Signature validation failed on token request #{request_num}: #{error}")
     end
   end
 end

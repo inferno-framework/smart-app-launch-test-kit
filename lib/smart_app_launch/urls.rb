f9a994f3a8cd8fbbe4f6aa0bb5ee57bc29adcb59
Karl Naden <knaden@mitre.org>
2025-04-02 11:18:55 -0400

    fix

--- lib/smart_app_launch/urls.rb
+++ lib/smart_app_launch/urls.rb
@@ -10,27 +10,27 @@ module SMARTAppLaunch
 
   module URLs
     def client_base_url
-      @base_url ||= "#{Inferno::Application['base_url']}/custom/#{client_suite_id}"
+      @client_base_url ||= "#{Inferno::Application['base_url']}/custom/#{client_suite_id}"
     end
 
     def client_fhir_base_url
-      @fhir_base_url ||= client_base_url + FHIR_PATH
+      @client_fhir_base_url ||= client_base_url + FHIR_PATH
     end
 
     def client_resume_pass_url
-      @resume_pass_url ||= client_base_url + RESUME_PASS_PATH
+      @client_resume_pass_url ||= client_base_url + RESUME_PASS_PATH
     end
 
     def client_resume_fail_url
-      @resume_fail_url ||= client_base_url + RESUME_FAIL_PATH
+      @client_resume_fail_url ||= client_base_url + RESUME_FAIL_PATH
     end
 
     def client_smart_discovery_url
-      @smart_discovery_url ||= client_base_url + SMART_DISCOVERY_PATH
+      @client_smart_discovery_url ||= client_base_url + SMART_DISCOVERY_PATH
     end
 
     def client_token_url
-      @token_url ||= client_base_url + TOKEN_PATH
+      @client_token_url ||= client_base_url + TOKEN_PATH
     end
 
     def client_suite_id
